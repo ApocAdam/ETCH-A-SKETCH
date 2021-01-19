661a12a95b166fb03b7f434591609fa960f52107
Adam <achappell2001@gmail.com>
2021-01-19 15:48:18 +1300

    Simple grid created

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 let divContainer = document.querySelector("#divContainer");
+let gridWidth = 4;
+let gridHeight = 4;
+
+// Creating div grid
+
+for (let i = 0; i < gridHeight; i++) {
+    let div = document.createElement("div");
+    div.classList.toggle("gridRow")
+    divContainer.appendChild(div);
+    for (let j = 0; j < gridWidth; j++) {
+        let div2 = document.createElement("div");
+        div2.classList.toggle("gridCell")
+        div.appendChild(div2);        
+    }
+}
