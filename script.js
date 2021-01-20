21b025eed83e4c27842a9d0268de8d740f21c51f
ApocAdam <40686539+ApocAdam@users.noreply.github.com>
2021-01-20 16:13:47 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
 let divContainer = document.querySelector("#divContainer");
-let gridWidth = 10; // make same as gh
-let gridHeight = gridWidth;
+let gridWidth = 4; // make same as gh
 
-let dCHeight = window.innerHeight;
-let dCWidth = window.innerWidth;
-let dC = Math.min(dCHeight, dCWidth)*0.6
+function resizeGrid(gW) {
+    let dCHeight = window.innerHeight;
+    let dCWidth = window.innerWidth;
+    let dC = Math.min(dCHeight, dCWidth)*0.8
 
-divContainer.style.height = `${dC+3*gridWidth}px`;
-divContainer.style.width = `${dC+3*gridHeight}px`; // need to scaling with border with
-// Creating div grid
+    divContainer.style.height = `${dC}px`;
+    divContainer.style.width = `${dC}px`;
 
-for (let i = 0; i < gridHeight; i++) {
-    let div = document.createElement("div");
-    div.classList.toggle("gridColumn")
-    divContainer.appendChild(div);
-    for (let j = 0; j < gridWidth; j++) {
-        let div2 = document.createElement("div");
-        div2.classList.toggle("gridCell")
-        div2.style.height = `${(dC)/gridWidth}px`;
-        div2.style.width = `${(dC)/gridWidth}px`;
-        div.appendChild(div2);        
+    for (let i = 0; i < gW; i++) {
+        let div = document.createElement("div");
+        div.classList.toggle("gridColumn")
+        divContainer.appendChild(div);
+        for (let j = 0; j < gW; j++) {
+            let div2 = document.createElement("div");
+            div2.classList.toggle("gridCell")
+            div2.style.height = `${-3+(dC)/gW}px`;
+            div2.style.width = `${-3+(dC)/gW}px`;
+            div.appendChild(div2);        
+        }
     }
 }
+
+resizeGrid(gridWidth);
+
+let dimension = document.querySelector(".slider");
+console.log(dimension.value);
+dimension.addEventListener("change", () => {
+    divContainer.innerHTML = "";
+    resizeGrid(dimension.value)});
