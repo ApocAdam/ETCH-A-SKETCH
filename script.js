2db12bd2fa96ac7852ba072d4918dbc4ec14ee0d
Adam <achappell2001@gmail.com>
2021-01-21 11:34:20 +1300

    Random colour trail which fades away. Completed for the most part

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let divContainer = document.querySelector("#divContainer");
-let gridWidth = 4; // make same as gh
+let gridWidth = 6; // make same as gh
 
 function resizeGrid(gW) {
     let dCHeight = window.innerHeight;
@@ -16,6 +16,12 @@ function resizeGrid(gW) {
         for (let j = 0; j < gW; j++) {
             let div2 = document.createElement("div");
             div2.classList.toggle("gridCell")
+            div2.addEventListener("mouseover", () => {
+                div2.style.backgroundColor = randomRGB();
+                setTimeout(() => {
+                    div2.style.backgroundColor = "white";
+                },1000);
+            })
             div2.style.height = `${-3+(dC)/gW}px`;
             div2.style.width = `${-3+(dC)/gW}px`;
             div.appendChild(div2);        
@@ -24,6 +30,13 @@ function resizeGrid(gW) {
 }
 
 resizeGrid(gridWidth);
+
+function randomRGB() {
+    let o = Math.round, r = Math.random, s = 255;
+    return 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+
+}
+console.log(randomRGB());
 
 let dimension = document.querySelector(".slider");
 let dimensionText = document.getElementById("text")
